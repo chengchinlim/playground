9b24c8a1602d0ade001d73961a4cc7c29e5b4087
Cheng Chin Lim <chengchin5227@gmail.com>
2024-04-05 10:16:22 -0700

    update tests with create product function

--- src/test/product.spec.ts
+++ src/test/product.spec.ts
@@ -1,29 +1,27 @@
 import { TestUtil } from "./util";
-import { DataHelper } from "./data.helper";
 import { ProductService } from "../product/product.service";
 
 describe("Product", () => {
   jest.setTimeout(20000);
 
   let testUtil: TestUtil;
-  let dataHelper: DataHelper;
 
   beforeAll(async () => {
     testUtil = new TestUtil();
     await testUtil.setupEnv();
-    dataHelper = new DataHelper(testUtil.dataSource);
   });
 
   beforeEach(async () => {
     await testUtil.clearDbData();
   });
 
-  it("Should get product", async () => {
-    const product = await dataHelper.addProduct();
+  it("Add & get product", async () => {
     const productService = testUtil.app.get(ProductService);
+    const product = await productService.addProduct("Product 1", "Category 1");
     const result = await productService.getProductById(product.id);
-    expect(result?.id).toBe(product.id);
-    expect(result?.name).toBe(product.name);
+    expect(result).toBeDefined();
+    expect(result!.id).toBe(product.id);
+    expect(result!.name).toBe(product.name);
   });
 
   afterAll(async () => {
